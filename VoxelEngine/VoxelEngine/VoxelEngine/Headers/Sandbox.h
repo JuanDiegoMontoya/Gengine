be3c78c05b8dd12cbbfe13dfce75b81c207f25a2
Colton Worrell <35385582+horsesNhalo@users.noreply.github.com>
2020-10-04 19:11:00 -0700

    nothing

--- VoxelEngine/VoxelEngine/VoxelEngine/Headers/Sandbox.h
+++ VoxelEngine/VoxelEngine/VoxelEngine/Headers/Sandbox.h
@@ -1,6 +1,6 @@
 #pragma once
 
-//#define SANDBOX
+#define SANDBOX
 
 //#include <boost/preprocessor/slot/counter.hpp>
 //#include <boost/preprocessor/cat.hpp>
@@ -40,8 +40,8 @@
 //
 //
 
-void Sandbox()
-{
+//void Sandbox()
+//{
   //std::string tester[5] = {
   //  DO_SOMETHING_COOL("a", "b", "c", "d"), "e"
   //};
@@ -71,4 +71,59 @@ void Sandbox()
   // a = 3
   // b = foo
 
+//}
+
+#include "../3rdParty/entt/src/entt.hpp"
+#include <cstdint>
+
+struct position {
+  float x;
+  float y;
+};
+
+struct velocity {
+  float dx;
+  float dy;
+};
+
+void update(entt::registry& registry) {
+  auto view = registry.view<position, velocity>();
+
+  for (auto entity : view) {
+    // gets only the components that are going to be used ...
+
+    auto& vel = view.get<velocity>(entity);
+
+    vel.dx = 0.;
+    vel.dy = 0.;
+
+    // ...
+  }
+}
+
+void update(std::uint64_t dt, entt::registry& registry) {
+  registry.view<position, velocity>().each([dt](auto& pos, auto& vel) {
+    // gets all the components of the view at once ...
+
+    pos.x += vel.dx * dt;
+    pos.y += vel.dy * dt;
+
+    // ...
+    });
+}
+
+
+void Sandbox()
+{
+  entt::registry registry;
+  std::uint64_t dt = 16;
+
+  for (auto i = 0; i < 10; ++i) {
+    auto entity = registry.create();
+    registry.emplace<position>(entity, i * 1.f, i * 1.f);
+    if (i % 2 == 0) { registry.emplace<velocity>(entity, i * .1f, i * .1f); }
+  }
+
+  update(dt, registry);
+  update(registry);
 }
