a929b4a41dff48730bbfdab18d97a9d5884db05a
Chris Thomas <chris@tyamoe.com>
2020-10-15 15:27:49 -0700

    puawhiofgeihe

--- VoxelEngine/VoxelEngine/VoxelEngine/CoreEngine/Window.h
+++ VoxelEngine/VoxelEngine/VoxelEngine/CoreEngine/Window.h
@@ -1,2 +1,38 @@
 #pragma once
 
+#include <iostream>
+
+#include "WindowUtils.h"
+
+struct GLFWwindow;
+
+class Window
+{
+public:
+	Window(const char* pName, Settings pSettings, Layout pLayout);
+	~Window();
+
+	bool Focus();
+
+	GLFWwindow* GetGLFWWindow() { return window; }
+
+	Layout GetLayout() { return layout; }
+	Layout& GetLayoutRef() { return layout; }
+
+	bool IsDirtyLayout() { return DirtyLayout; }
+
+private:
+	std::string name;
+
+	bool WindowInitialized = false;
+	bool DirtyLayout = false;
+
+	Settings settings;
+	Layout layout;
+
+	void MakeWindow();
+
+protected:
+	GLFWwindow* window = nullptr;
+
+};
---
+++ VoxelEngine/VoxelEngine/VoxelEngine/CoreEngine/WindowCB.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+#include "WindowCB.h"
+
+void ViewportCB(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+
+	//WindowManager[window]->rLayout().width = width;
+	//WindowManager[window]->rLayout().height = height;
+	//WindowManager[window]->rDirtyLayout() = true;
+}
+
+void ErrorCB(int error, char const* description)
+{
+	std::cerr << "GLFW error: " << description << std::endl;
+}
+
+void GLAPIENTRY ExErrorCB(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam)
+{
+	if (id == 131169 || id == 131185 || id == 131218 || id == 131204) return;
+
+	std::cout << "---------------" << std::endl;
+	std::cout << "Debug message (" << id << "): " << message << std::endl;
+
+	switch (source)
+	{
+	case GL_DEBUG_SOURCE_API:             std::cout << "Source: API"; break;
+	case GL_DEBUG_SOURCE_WINDOW_SYSTEM:   std::cout << "Source: Window System"; break;
+	case GL_DEBUG_SOURCE_SHADER_COMPILER: std::cout << "Source: Shader Compiler"; break;
+	case GL_DEBUG_SOURCE_THIRD_PARTY:     std::cout << "Source: Third Party"; break;
+	case GL_DEBUG_SOURCE_APPLICATION:     std::cout << "Source: Application"; break;
+	case GL_DEBUG_SOURCE_OTHER:           std::cout << "Source: Other"; break;
+	} std::cout << std::endl;
+
+	switch (type)
+	{
+	case GL_DEBUG_TYPE_ERROR:               std::cout << "Type: Error"; break;
+	case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: std::cout << "Type: Deprecated Behaviour"; break;
+	case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:  std::cout << "Type: Undefined Behaviour"; break;
+	case GL_DEBUG_TYPE_PORTABILITY:         std::cout << "Type: Portability"; break;
+	case GL_DEBUG_TYPE_PERFORMANCE:         std::cout << "Type: Performance"; break;
+	case GL_DEBUG_TYPE_MARKER:              std::cout << "Type: Marker"; break;
+	case GL_DEBUG_TYPE_PUSH_GROUP:          std::cout << "Type: Push Group"; break;
+	case GL_DEBUG_TYPE_POP_GROUP:           std::cout << "Type: Pop Group"; break;
+	case GL_DEBUG_TYPE_OTHER:               std::cout << "Type: Other"; break;
+	} std::cout << std::endl;
+
+	switch (severity)
+	{
+	case GL_DEBUG_SEVERITY_HIGH:         std::cout << "Severity: high"; break;
+	case GL_DEBUG_SEVERITY_MEDIUM:       std::cout << "Severity: medium"; break;
+	case GL_DEBUG_SEVERITY_LOW:          std::cout << "Severity: low"; break;
+	case GL_DEBUG_SEVERITY_NOTIFICATION: std::cout << "Severity: notification"; break;
+	} std::cout << std::endl;
+	std::cout << std::endl;
+}
