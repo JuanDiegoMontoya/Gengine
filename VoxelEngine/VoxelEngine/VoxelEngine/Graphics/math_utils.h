71ad3f12f1e1ac08118f4d9e403fbf9da6c9715a
Jake <jake.ryan@digipen.edu>
2020-10-15 17:29:10 -0700

    Implemented reversed Z-buffer.

--- VoxelEngine/VoxelEngine/VoxelEngine/Graphics/math_utils.h
+++ VoxelEngine/VoxelEngine/VoxelEngine/Graphics/math_utils.h
@@ -1,5 +1,5 @@
 #pragma once
-#include <Graphics/GraphicsIncludes.h>
+#include <MathIncludes.h>
 
 namespace Utils
 {
@@ -58,5 +58,15 @@ namespace Utils
     int y = index / w;
     int x = index % w;
     return glm::vec3(x, y, z);
+  }
+
+  inline glm::mat4 MakeInfReversedZProjRH(float fovY_radians, float aspectWbyH, float zNear)
+  {
+    float f = 1.0f / tan(fovY_radians / 2.0f);
+    return glm::mat4(
+      f / aspectWbyH, 0.0f, 0.0f, 0.0f,
+      0.0f, f, 0.0f, 0.0f,
+      0.0f, 0.0f, 0.0f, -1.0f,
+      0.0f, 0.0f, zNear, 0.0f);
   }
 }
