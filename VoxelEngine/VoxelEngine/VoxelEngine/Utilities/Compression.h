118c3bac928183c20f465818d82d00c752bbb9dd
Jake <jake.ryan@digipen.edu>
2020-11-22 22:52:38 -0800

    Merged compression stuff into one file.

--- VoxelEngine/VoxelEngine/VoxelEngine/Utilities/Compression.h
+++ VoxelEngine/VoxelEngine/VoxelEngine/Utilities/Compression.h
@@ -9,6 +9,9 @@
 
 namespace Compression
 {
+  /******************************************************************************
+  ************************** zlib compression ***********************************
+  ******************************************************************************/
   // templated for type safety
   template<typename T>
   struct CompressionResult
@@ -69,5 +72,86 @@ namespace Compression
 
     uncompressedData.assign((T*)uncompressedDataTemp.get(), (T*)uncompressedDataTemp.get() + uncompressedSize / sizeof(T));
     return uncompressedData;
+  }
+
+  /******************************************************************************
+  ************************** RLE encoding ***************************************
+  ******************************************************************************/
+  template<typename T>
+  struct RLEelement
+  {
+    bool operator==(const RLEelement<T> rhs) const
+    {
+      return count == rhs.count && value == rhs.value;
+    }
+    uint32_t count{};
+    T value{};
+  };
+
+  template<typename T>
+  std::vector<RLEelement<T>> EncodeRLE(std::span<T> array)
+  {
+    std::vector<RLEelement<T>> data;
+
+    RLEelement<T> curElement{ .value = array[0] };
+    for (auto value : array)
+    {
+      if (value != curElement.value)
+      {
+        data.push_back(curElement);
+        curElement = RLEelement<T>{ .count = 0, .value = value };
+      }
+      curElement.count++;
+    }
+    if (curElement.count > 0)
+      data.push_back(curElement);
+
+    return data;
+  }
+
+  template<typename T>
+  std::vector<T> DecodeRLE(std::span<RLEelement<T>> data)
+  {
+    std::vector<T> array;
+
+    for (auto rlee : data)
+    {
+      array.insert(array.end(), rlee.count, rlee.value);
+    }
+
+    return array;
+  }
+
+/******************************************************************************
+************************** Delta encoding *************************************
+******************************************************************************/
+  //template<std::signed_integral T>
+  template<typename T>
+  std::vector<T> EncodeDelta(std::span<T> array)
+  {
+    std::vector<T> encoded;
+    T prev{ 0 };
+    for (T current : array)
+    {
+      T delta = current - prev;
+      prev = current;
+      encoded.push_back(delta);
+    }
+    return encoded;
+  }
+
+  //template<std::signed_integral T>
+  template<typename T>
+  std::vector<T> DecodeDelta(std::span<T> encoded)
+  {
+    std::vector<T> decoded;
+    T prev{ 0 };
+    for (T delta : encoded)
+    {
+      T current = delta + prev;
+      prev = current;
+      decoded.push_back(current);
+    }
+    return decoded;
   }
 }
