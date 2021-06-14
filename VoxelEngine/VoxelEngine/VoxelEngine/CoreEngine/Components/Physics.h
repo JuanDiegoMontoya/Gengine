f45d437244450adede6ba0a6883df3a073abe695
Jake <jake.ryan@digipen.edu>
2021-06-14 01:21:16 -0700

    Removed obsolete code.

--- VoxelEngine/VoxelEngine/VoxelEngine/CoreEngine/Components/Physics.h
+++ VoxelEngine/VoxelEngine/VoxelEngine/CoreEngine/Components/Physics.h
@@ -14,23 +14,12 @@ namespace Component
       internalActor = ::Physics::PhysicsManager::AddDynamicActorEntity(ent, mat, c, flags);
     }
 
-    //DynamicPhysics(DynamicPhysics&& rhs) noexcept { *this = std::move(rhs); }
-    //DynamicPhysics& operator=(DynamicPhysics&& rhs) noexcept
-    //{
-    //  if (&rhs == this) return *this;
-    //  internalActor = std::exchange(rhs.internalActor, nullptr);
-    //  return *this;
-    //}
-    //DynamicPhysics(const DynamicPhysics&) = delete;
-    //DynamicPhysics& operator=(const DynamicPhysics&) = delete;
-
     ::Physics::DynamicActorInterface Interface()
     {
       return internalActor;
     }
 
     physx::PxRigidDynamic* internalActor;
-
   };
 
   struct StaticPhysics
@@ -44,68 +33,26 @@ namespace Component
       internalActor = ::Physics::PhysicsManager::AddStaticActorEntity(ent, mat, c);
     }
 
-    StaticPhysics(StaticPhysics&& rhs) noexcept { *this = std::move(rhs); }
-    StaticPhysics& operator=(StaticPhysics&& rhs) noexcept
-    {
-      internalActor = std::exchange(rhs.internalActor, nullptr);
-      return *this;
-    }
-    StaticPhysics(const StaticPhysics&) = delete;
-    StaticPhysics& operator=(const StaticPhysics&) = delete;
-
     ::Physics::StaticActorInterface Interface()
     {
       return internalActor;
     }
 
-    //~StaticPhysics()
-    //{
-    //  if (internalActor)
-    //  {
-    //    //printf("PHYSICS %p really DELETED\n", this);
-    //    ::Physics::PhysicsManager::RemoveActorEntity(reinterpret_cast<physx::PxRigidActor*>(internalActor));
-    //  }
-    //}
-
-  //private:
     physx::PxRigidStatic* internalActor;
   };
 
   struct CharacterController
   {
-    //CharacterController(Entity ent, ::Physics::MaterialType mat, const ::Physics::BoxCollider& c)
-    //{
-    //  internalController = ::Physics::PhysicsManager::AddCharacterControllerEntity(ent, mat, c);
-    //}
     CharacterController(Entity ent, ::Physics::MaterialType mat, const ::Physics::CapsuleCollider& c)
     {
       internalController = ::Physics::PhysicsManager::AddCharacterControllerEntity(ent, mat, c);
     }
-
-    //CharacterController(CharacterController&& rhs) noexcept { *this = std::move(rhs); }
-    //CharacterController& operator=(CharacterController&& rhs) noexcept
-    //{
-    //  internalController = std::exchange(rhs.internalController, nullptr);
-    //  return *this;
-    //}
-    //CharacterController(const CharacterController&) = delete;
-    //CharacterController& operator=(const CharacterController&) = delete;
 
     ::Physics::CharacterControllerInterface Interface()
     {
       return internalController;
     }
 
-    //~CharacterController()
-    //{
-    //  if (internalController)
-    //  {
-    //    //printf("PHYSICS %p really DELETED\n", this);
-    //    ::Physics::PhysicsManager::RemoveCharacterControllerEntity(internalController);
-    //  }
-    //}
-
-  //private:
     physx::PxController* internalController;
   };
 }
